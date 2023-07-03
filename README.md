9483c42c4345bd7ce24c336f34f577b31775c725
Jack <jackwebdev31@gmail.com>
2023-07-03 07:43:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Launch countdown timer
+
+![Design preview for the Launch countdown timer coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for all interactive elements on the page
+- See a live countdown timer that ticks down every second
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@yourusername](https://www.twitter.com/yourusername)
---
+++ index.js
@@ -0,0 +1,29 @@
+
+const days = document.querySelector(".days h2");
+const hours = document.querySelector(".hours h2");
+const minutes = document.querySelector(".minutes h2");
+const seconds = document.querySelector(".seconds h2");
+
+const launchCountDown = () => {
+  const launchDate = new Date("August 12 2023 00:00:00").getTime();
+  const currentDate = new Date().getTime();
+
+  const countDownTime = launchDate - currentDate;
+
+  const calcSeconds = 1000;
+  const calcMinutes = calcSeconds * 60;
+  const calcHours = calcMinutes * 60;
+  const calcDays = calcHours * 24;
+
+  const launchDays = Math.floor(countDownTime / calcDays);
+  const launchHours = Math.floor((countDownTime % calcDays) / calcHours);
+  const launchMinutes = Math.floor((countDownTime % calcHours) / calcMinutes);
+  const launchSeconds = Math.floor((countDownTime % calcMinutes) / calcSeconds);
+
+  days.innerText = launchDays;
+  hours.innerText = launchHours;
+  minutes.innerText = launchMinutes;
+  seconds.innerText = launchSeconds;
+};
+
+setInterval(launchCountDown, 1000);
